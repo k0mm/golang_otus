4a320495043bba98e895e1765b9326ccc491a100
p.yuriev <p.yuriev@crpt.ru>
2021-07-15 17:38:22 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,7 +7,7 @@ import (
 	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var ErrInvalidString = errors.New("Invalid string!!!")
 
 func nextRune(index int, slice []rune) rune {
 	value := '1'
